cb05e179da75066bc075768bc00251a7fae11376
ED-Annadanam <engdesign12e@gmail.com>
2022-06-24 19:02:06 +0530

    email added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Members(models.Model):
+  supplier_or_receiver=models.CharField(max_length=8)
+  email_id = models.CharField(max_length=255)
+  # Create your models here.
--- users/views.py
+++ users/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
-
-
-
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
-
+import HttpResponseRedirect
+import reverse
 # Create your views here.
  
 def home(request):
     return render(request,"users/home.html")
 
 
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
 
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
